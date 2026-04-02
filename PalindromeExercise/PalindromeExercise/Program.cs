8fb30c2a4a78f15bdcf18974e2959ffc2dce3c53
Theo Boateng <Theoboateng2002@gmail.com>
2026-04-02 12:45:34 -0400

    intial commit

--- PalindromeExercise/PalindromeExercise/Program.cs
+++ PalindromeExercise/PalindromeExercise/Program.cs
@@ -2,11 +2,26 @@
 
 namespace PalindromeExercise
 {
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            string reversedWord = "";
+
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversedWord += word[i];
+            }
+
+            return word.ToLower() == reversedWord.ToLower();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Palindrome Exercise");
         }
     }
 }
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,27 @@
-using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("madam", true)]
+        [InlineData("level", true)]
+        [InlineData("hello", false)]
+        [InlineData("world", false)]
+        [InlineData("RaceCar", true)]
+        public void IsAPalindrome_GivenAWord_ReturnsExpectedResult(string word, bool expected)
         {
+            // Arrange
+            WordSmith wordSmith = new WordSmith();
 
+            // Act
+            bool actual = wordSmith.IsAPalindrome(word);
+
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
